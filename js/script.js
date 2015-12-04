f7ed36659488abf93488c3e22996a2cf4ac6d91d
Felipe Uliana <v.uliana@gmail.com>
2015-12-04 09:23:58 -0200

    [checked] Get the NYTimes Article Search API Response

--- js/script.js
+++ js/script.js
@@ -11,15 +11,25 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
     // YOUR CODE GOES HERE!
+
+    // load streetview
     var $street = $('#street').val();
     var $city = $('#city').val();
 
     var streetViewAddress = $street + ', ' + $city;
 
     $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + streetViewAddress + '">');
+
+    // load New York Times api
+    var nytApiUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json';
+    var nytQuery = '?q=' + $street + '+' + $city;
+    var nytApiKey = '&api-key=82bd23bcb69609f99ac4a626ab4d5216:14:73655973';
+    var nytUrl = nytApiUrl + nytQuery + nytApiKey;
+
+    $.getJSON(nytUrl, function(data) {
+        console.log(data);
+    });
 
     return false;
 };
